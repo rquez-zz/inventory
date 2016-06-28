d73b3fa7816bfd4a24cc0707836f0b9885b03545
rquez <rvzxjr@gmail.com>
2016-06-28 13:07:31 -0400

    Register hapi-router and blipp

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const server = new Hapi.Server();
 
 server.connection(config.connection);
 
+server.register([
+{
+    register: require('blipp'),
+    options: { showAuth: true }
+},
+{
+    register: require('hapi-router'),
+    options: { routes: './app/routes/*.js' }
+}], (err) => {
+    if (err) {
+        console.log('Error registering hapi plugins');
+        throw err;
+    }
+});
+
 server.start(() => {
     console.log('Server running at: ' + config.connection.port);
 });
